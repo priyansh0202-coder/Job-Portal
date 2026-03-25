6a92a69c12d8483ff5af738c597c654cbfef3ce1
priyanshinfusyx <infusyx.priyansh@gmail.com>
2026-03-25 22:25:20 +0530

    feat: remove all console in authlogin

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -6,7 +6,6 @@ const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
     const [user, setUser] = useState(null);
-    console.log(user, "auth")
 
     useEffect(() => {
         setUser(getStoredUser());
